d2dcf8e79394689b88cf5d1bbfa4f29e4f755d58
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-03-26 12:23:57 +0200

    fix the issue

--- task.sh
+++ task.sh
@@ -1,11 +1,24 @@
 #! /bin/bash
-set -e
+mysqldump -u"$DB_USER" -p"$DB_PASSWORD" ShopDB --result-file=ShopDB_full_backup.sql
+if [ $? -ne 0 ]; then
+  echo "Backup failed!"
+  exit 1
+fi
 
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --result-file=ShopDB_full_backup.sql
 mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDB_full_backup.sql
+if [ $? -ne 0 ]; then
+  echo "Restore failed"
+  exit 1
+fi
 
 
-DATA=$(mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDB -e "SHOW TABLES;" --skip-column-names)
-
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB $DATA > ShopDB_data_backup.sql
-mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < ShopDB_data_backup.sql
+mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB --result-file=data_backup.sql
+if [ $? -ne 0 ]; then
+  echo "Backup failed!"
+  exit 1
+fi
+mysql -u"$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data_backup.sql
+if [ $? -ne 0 ]; then
+  echo "Restore failed!"
+  exit 1
+fi
